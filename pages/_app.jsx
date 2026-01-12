a958fd5d4e598d71a70f8574cfcffa7d0c483456
giuseppe101-arch <northwayengineeringltd@gmail.com>
2026-01-12 20:40:24 +0000

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,20 +1,17 @@
-import Head from 'next/head'
-import '../styles/globals.css'
+import Head from "next/head";
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
         <meta
           name="google-site-verification"
-          content="<meta name="google-site-verification" content="EkMwvXzFzU7eWlt5qCEn3hc1Jp_d2db1hDayxg_UJNY" />"
+          content="EkMwvXzFzU7eWlt5qCEn3hc1Jp_d2db1hDayxg_UJNY"
         />
       </Head>
 
       <Component {...pageProps} />
     </>
-  )
+  );
 }
-
-export default MyApp
-
