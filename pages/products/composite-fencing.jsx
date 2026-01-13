d56e037a20e6f3dbdb70236d5417af72dd990bcd
giuseppe101-arch <northwayengineeringltd@gmail.com>
2026-01-13 13:29:27 +0000

    Update composite-fencing.jsx

--- pages/products/composite-fencing.jsx
+++ pages/products/composite-fencing.jsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+import BreadcrumbSchema from "../../components/BreadcrumbSchema";
 
 export default function CompositeFencingPage() {
   // FAQ schema must match visible FAQ content on the page
@@ -64,6 +65,18 @@ export default function CompositeFencingPage() {
           dangerouslySetInnerHTML={{ __html: JSON.stringify(FAQ_SCHEMA) }}
         />
       </Head>
+
+      {/* Breadcrumb Schema */}
+      <BreadcrumbSchema
+        items={[
+          { name: "Home", item: "https://www.northwayengineering.co.uk/" },
+          { name: "Products", item: "https://www.northwayengineering.co.uk/#services" },
+          {
+            name: "Composite Fencing",
+            item: "https://www.northwayengineering.co.uk/products/composite-fencing",
+          },
+        ]}
+      />
 
       <Header />
 
@@ -319,3 +332,4 @@ export default function CompositeFencingPage() {
     </>
   );
 }
+
