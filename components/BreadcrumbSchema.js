2de9266f4d0311a7a00b00588808394e40922932
giuseppe101-arch <northwayengineeringltd@gmail.com>
2026-01-13 13:14:34 +0000

    Update BreadcrumbSchema.js

--- components/BreadcrumbSchema.js
+++ components/BreadcrumbSchema.js
@@ -1,1 +1,26 @@
+import Head from "next/head";
+
+export default function BreadcrumbSchema({ items = [] }) {
+  if (!items.length) return null;
+
+  const schema = {
+    "@context": "https://schema.org",
+    "@type": "BreadcrumbList",
+    itemListElement: items.map((it, index) => ({
+      "@type": "ListItem",
+      position: index + 1,
+      name: it.name,
+      item: it.item,
+    })),
+  };
+
+  return (
+    <Head>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
+      />
+    </Head>
+  );
+}
 
